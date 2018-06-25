31d5f56c53754ba4d237d0c6562a6ba0b7970e36
sub-v <15256025969@163.com>
2018-06-25 18:00:10 +0800

    头部部分提交

---
+++ public/js/public.js
@@ -0,0 +1,29 @@
+function initNav () { 
+  var sTop = $(window).scrollTop();  
+  sTop = parseInt(sTop);
+  if (sTop >= 10) {
+  	$(".global-body, .header-wrapper, .header-logo").addClass('win-nav');
+  } else { 
+  	$(".global-body, .header-wrapper, .header-logo").removeClass('win-nav');
+  } 
+}
+function setWinNav () {
+	$(window).bind("scroll", initNav);
+}
+function setNavItemStyl () {
+	var navIdx = $("#nav-item-index").val();
+	var navItem = $(".header-nav-item");
+	navItem.find('.nav-item-link').removeClass('now-link');
+	navItem.each(function () {
+		var itemIdx = $(this).attr("itemIdx");
+		if (navIdx === itemIdx && navIdx !== undefined && itemIdx !== undefined) {
+			$(this).find('.nav-item-link').addClass('now-link');
+		};
+	});
+}
+
+$(function () {
+	initNav();
+	setNavItemStyl();
+	setWinNav();
+})
