cc6c9fc9f9b648609ef3c85f7e27408c008f822b
subBoy <15256025969@163.com>
2018-07-08 16:33:55 +0800

    说明等文件

--- public/js/broadside.js
+++ public/js/broadside.js
@@ -1,9 +1,12 @@
-function showSlide () {
+function showSlide (callback) {
 	var sTop = $(window).scrollTop();
 	if (sTop >= 350) {
 		$('.ju-silde-wrapper').fadeIn();
 	} else {
 		$('.ju-silde-wrapper').hide();
+	}
+	if (callback) {
+		callback();
 	}
 }
 function slideAn () {
@@ -11,32 +14,30 @@ function slideAn () {
 	$('.ju-silde-item').on('click', function () {
 		clearTimeout(timer);
 		timer = setTimeout(function () {
-			scrollToId();
+			scrollToId(0);
 		}, 100)
 	})
 }
-function scrollToId () {
+function scrollToId (status) {
 	var url = location.href;
-	console.log(url);
 	var id = url.split('#')[1];
 	var targetId = $('.' + id);
 	if (id && targetId.length) {
 		var sTop = targetId.offset().top;
 		$("html,body").animate({
-			scrollTop: sTop - 110 + 'px'
+			scrollTop: sTop - 110 - status + 'px'
 		}, 500);
 		var slide = $('.ju-silde-item');
 		$.each(slide, function () {
-			var slideId = $(this).find('.ju-silde-link').attr('href');
-			if (slideId === '#' + id) {
+			var slideId = $(this).find('.ju-silde-link').attr('href').split('#')[1];
+			if (slideId === id) {
 				$(this).addClass('now-station').siblings().removeClass('now-station');
 			}
 		})
 	}
 }
 $(function () {
-	scrollToId();
+	scrollToId(70);
 	showSlide();
-	$(window).bind("scroll", showSlide);
 	slideAn();
 })
