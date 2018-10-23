940e9fca0c757e4271fecd9c44ea0823854ba8b2
subBoy <15256025969@163.com>
2018-10-23 23:40:10 +0800

    201810232340

--- home-of/groove.js
+++ home-of/groove.js
@@ -22,7 +22,26 @@ function setPl () {
 		$('#font-length').html(len + '/200字');
 	})
 }
+function delgeTab () {
+	var idx;
+	$('.getb-del-btn-wrapper').on('click', function () {
+		idx = $(this).parent().parent().parent().index();
+		$('.groove-table-win-wrapper').fadeIn();
+	})
+	$('.groove-table-win-close-btn').on('click', function () {
+		$('.groove-table-win-wrapper').fadeOut();
+	})
+	$('.groove-table-win-sumbit-btn').on('click', function () {
+		$('.groove-table-itrm').eq(idx).remove();
+		$('.groove-table-win-wrapper').fadeOut();
+	})
+	$('.groove-item-link').on('click', function () {
+		$('#groove-table-content').hide();
+		$('#groove-list-content').fadeIn();
+	})
+}
 $(function () {
 	getNowTime();
 	setPl();
+	delgeTab();
 })
